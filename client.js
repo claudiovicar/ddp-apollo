6963e788dd4292c6285e5f0113a08f2519e72341
claudio.carvalho <claudiovicar@gmail.com>
2024-10-10 13:29:36 -0300

    extract apollo-link-ddp

--- client.js
+++
@@ -1,6 +0,0 @@
-import { DDPLink } from '@swydo/apollo-link-ddp';
-
-export * from '@swydo/apollo-link-ddp';
-
-// It is common for Apollo Links to export the link itself as the default
-export default DDPLink;
